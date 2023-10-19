6aa50a907c7fa120dc472b9ad6b0b0f8b701a739
Quodlun <willyfu123@gmail.com>
2023-10-19 11:44:48 +0800

    10/19/2023 11:44

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Version content
 > Version: Textbook / Workbook
 
-* 2023.1019.0: P89 / N/A
+* 2023.1019.0: P111 / N/A
+* 2023.1019.1: P115 / N/A
--- cpld_code.vhd
+++ cpld_code.vhd
@@ -8,15 +8,17 @@ port
 (
 	A	: in std_logic;
 	B	: in std_logic;
+	Ci	: in std_logic;
 	
 	S	: out std_logic;
-	C	: out std_logic
+	Co	: out std_logic
 );
 
 end cpld_code;
 
 architecture df of cpld_code is
 begin
-	S <= A XOR B;
-	C <= A AND B;	
+	S <= A XOR B XOR Ci;
+	Co <= ( A AND B ) OR ( B AND Ci ) OR ( A AND Ci );	
+	
 end df;
