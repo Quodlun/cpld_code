d5279041990033d9a1f60423f7a133fc0f6ad109
Quodlun <willyfu123@gmail.com>
2023-11-23 11:15:19 +0800

    11/23/2023 11:15

--- cpld_code.vhd
+++ cpld_code.vhd
@@ -6,23 +6,23 @@ use ieee.std_logic_arith.all;
 entity cpld_code is
 port
 (
-	X	: in std_logic;
-	Y	: in std_logic;
-	Bi	: in std_logic;
+	sw_in	: in std_logic_vector ( 8 downto 0 );
 	
-	D	: out std_logic;
-	Bo	: out std_logic
+	bcd		: out std_logic_vector ( 3 downto 0 )
 );
 
 end cpld_code;
 
 architecture df of cpld_code is
 begin
-	process ( X, Y, Bi )
-	
-	begin
-		D <= X XOR Y XOR Bi;
-		Bo <= ( ( NOT X ) AND Y ) OR ( ( NOT X) AND Bi ) OR ( Y AND Bi );	
-	
-	end process;
+	bcd <= "1001" when ( sw_in ( 8 ) = '1' ) else
+		   "1000" when ( sw_in ( 7 ) = '1' ) else
+		   "0111" when ( sw_in ( 6 ) = '1' ) else
+		   "0110" when ( sw_in ( 5 ) = '1' ) else
+		   "0101" when ( sw_in ( 4 ) = '1' ) else
+		   "0100" when ( sw_in ( 3 ) = '1' ) else
+		   "0011" when ( sw_in ( 2 ) = '1' ) else
+		   "0010" when ( sw_in ( 1 ) = '1' ) else
+		   "0001" when ( sw_in ( 0 ) = '1' ) else
+		   "0000";
 end df;
